c8f97665724946cbf923a8fdb3b3e960459f67c6
Nestor Alvaro <nestoralvaro@gmail.com>
2011-04-21 01:20:58 +0200

    First beta (fix needed for androids)

---
+++ cache.manifest.php
@@ -0,0 +1,28 @@
+<?php
+    header('Content-Type: text/cache-manifest');
+?>
+CACHE MANIFEST
+
+# version0.2
+
+# Tells the web page to allow it to load anything not found in the cache.
+NETWORK:
+*
+CACHE:
+css/cards.css
+css/add2home.css
+images/iphone/favicon.ico
+images/iphone/startup.png
+images/iphone/touch-icon-ipad.png
+images/iphone/touch-icon-iphone.png
+images/iphone/touch-icon-iphone4.png
+images/background.png
+images/cards.png
+images/en.png
+images/es.png
+images/settings.png
+javascript/jquery-1.5.2.min.js
+javascript/percentajesPreFlop.js
+javascript/script.js
+javascript/add2home.js
+index.html
